61e4a7216d2d3d752a36c8c9e00b80c78237395e
SAKTHINATHAN V <sakthinathan3110@gmail.com>
2026-05-13 23:09:59 +0530

    Create 424. Longest Repeating Character Replacement.cpp

---
+++ Substring Patterns/Variable Size Sliding Window/424. Longest Repeating Character Replacement.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n = s.length();
+
+        int max_len = 0;
+        int max_freq = 0;
+
+        vector<int> freq(26, 0);
+
+        int left = 0;
+        for (int right = 0; right < n; right++) {
+            freq[s[right] - 'A']++;
+
+            max_freq = max(max_freq, freq[s[right] - 'A']);
+
+            while ((right - left + 1) - max_freq > k) {
+                freq[s[left] - 'A']--;
+                left++;
+            }
+            max_len = max(max_len, (right - left + 1));
+        }
+
+        return max_len;
+    }
+};
